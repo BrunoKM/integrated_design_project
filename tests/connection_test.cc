bdef90a511b025342404d7d35fdc19156ace1596
Bruno Kacper Mlodozeniec <bkmlodozeniec@gmail.com>
2018-02-22 09:38:16 +0000

    Fix bug in instruction_send_time_test

--- tests/connection_test.cc
+++ tests/connection_test.cc
@@ -8,10 +8,10 @@ robot_link rlink;      // datatype for the robot link
 int main () {
   int val;
   #ifdef __arm__
-    cout << "Running code on MICROCONTROLLER"
+    cout << "Running code on MICROCONTROLLER" << endl;
     if (!rlink.initialise ("127.0.0.1")) {          // setup for local hardware
   #else
-    cout << "Running code REMOTELY"
+    cout << "Running code REMOTELY" << endl;
     if (!rlink.initialise (ROBOT_NUM)) {            // setup the link
   #endif
     cout << "Cannot initialise link" << endl;
