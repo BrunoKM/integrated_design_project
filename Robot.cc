e75d98999c7466e81190621a45b59aa38ad9f459
Bruno Mlodozeniec <bkmlodozeniec@gmail.com>
2018-03-01 21:43:47 +0000

    Robot skeleton finished

--- Robot.cc
+++ Robot.cc
@@ -4,3 +4,66 @@
 // A to B, or dropping eggs off.
 //
 // Also contains helper class Eggs and struct Egg
+
+#include <string>
+#include <vector>
+#include "robot.h"
+
+Eggs::Eggs() {}
+
+void Eggs::add_egg(Egg egg) {
+
+}
+
+void Eggs::clear() {
+  // Clears all the eggs, i.e. no eggs in the eggs vector after this
+  // function is called.
+}
+
+
+// The Robot class methods
+Robot::Robot() {
+
+}
+
+void Robot::input_restart_parameters(int baskets_delivered, string delivery_zone) {
+  // Allows for key parameters to be reentered post-restart.
+}
+
+void Robot::move(string a, string b) {
+
+}
+
+void Robot::align_for_pickup() {
+
+}
+
+void Robot::pick_up_eggs(int num_to_recycle) {
+
+}
+
+void Robot::pick_up_all_eggs() {
+
+}
+void Robot::deliver_basket() {
+
+}
+
+void Robot::recycle_eggs() {
+
+}
+
+// Private Methods:
+
+int Robot::sort_egg(bool large_egg) {
+  // Returns 1 for egg sorted into delivery reservoir and 0 for egg sorted into recycling.
+}
+
+Egg Robot::classify_egg(bool large_egg) {
+  // Use the IR sensor and diode to classify the egg
+}
+
+void Robot::update_onboard_eggs(Egg egg, int reservoir) {
+  // Updates the delivery_eggs and recycling_eggs parameters of the object.
+  // Updates the LEDs displaying the composition.
+}
