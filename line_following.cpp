946bfa13101e21a1cd52665cbb8bf2e08de891eb
Holly <rowland.m.holly@gmail.com>
2018-03-05 12:36:24 +0000

    Added basic structure for line_following.cpp

---
+++ line_following.cpp
@@ -0,0 +1,70 @@
+#include <robot_link>
+#include "line_following.h"
+
+
+
+Line_Following::Line_Following(){}
+
+void Line_Following::follow_line(float speed, float ramp, int num_intersections_to_ignore,
+                                 bool keep_driving_after_last) {
+// Follows the line until a certain number of intersections has been passed
+};
+
+void Line_Following::align_with_intersection() {
+// Keep driving for a pre-calibrated amount until
+};
+
+void Line_Following::turn(int degrees) {
+
+};
+
+void Line_Following::follow_line_reverse(float speed, float ramp) {
+
+};
+
+int Line_Following::is_on_the_line() {
+
+};
+
+int Line_Following::wiggle_to_line() {
+
+};
+
+int Line_Following::reiterate_in_reverse(float reiterate_for) {
+
+};
+
+
+
+
+
+Line_Sensor_Reading::Line_Sensor_Reading(int sensor_reading){}
+
+int Line_Sensor_Reading::back_left() {
+
+};
+
+int Line_Sensor_Reading::back_right() {
+
+};
+
+int Line_Sensor_Reading::front_left() {
+
+};
+
+int Line_Sensor_Reading::front_right(){
+
+};
+
+
+
+
+
+Line_Sensors::Line_Sensors(int address){}
+
+Line_Sensor_Reading Line_Sensors::read_sensor_input() {
+
+};
+
+
+
--- line_following.h
+++ line_following.h
@@ -1,6 +1,8 @@
-// Declares the class Line_Following
+// Defines several classes for the main parts involved in line_following
+
 #ifndef LINE_FOLLOWING_H
 #define LINE_FOLLLOWING_H
+
 #include "Driving_Motor.h"
 #include "robot_initialise.h"
 
@@ -25,8 +27,11 @@ public:
 
     // Methods
     void follow_line(float speed, float ramp, int num_intersections_to_ignore, bool keep_driving_after_last);
+
     void align_with_intersection();
+
     void turn(int degrees);
+
     void follow_line_reverse(float speed, float ramp);
 
 };
@@ -34,24 +39,21 @@ public:
 
 class Line_Sensor_Reading {
 public:
-    Line_Sensor_Reading();
-
-    int sensor_reading;
+    Line_Sensor_Reading(int sensor_reading);
 
     // Sensor readings return 1 or 0
     int front_left();
+
     int front_right();
+
     int back_left();
+
     int back_right();
 };
 
 class Line_Sensors {
-
-private:
-    int address;
-
 public:
-    Line_Sensors();
+    Line_Sensors(int address;);
 
     // Methods
     Line_Sensor_Reading read_sensor_input();
