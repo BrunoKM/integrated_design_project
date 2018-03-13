8f30acd8ef2ad9d6b9d68d491931091d87b30ca9
Bruno K. Mlodozeniec <bkmlodozeniec@gmail.com>
2018-03-13 16:15:13 +0000

    Further changes to turntable comms test

--- test_turntable_comms.cpp
+++ test_turntable_comms.cpp
@@ -28,16 +28,24 @@ int main () {
 
   // stopwatch watch;
   // watch.start();
-  cout << "Set the angle to 0 to begin with." << endl;
-  components.turntable_comms.set_angle(0);
+  //cout << "Set the angle to 0 to begin with." << endl;
+  //components.turntable_comms.set_angle(0);
 
-  int delay_time = 2000;
-
-  for (int i = 0; i <= 255; i+=10) {
-    components.turntable_comms.set_angle(0);
-    cout << "Value sent to PIC: " << i << endl;
-    delay(delay_time);
-  }
+  int delay_time = 5000;
+  int counter = 0;
+	
+	while (true) {
+		//components.turntable_comms.set_angle(255);
+		rlink.command(WRITE_PORT_2, (counter * 10 )% 256);
+		cout << counter << ": Sent signal " << ((counter*10)% 256) << endl;
+		counter ++;
+		delay(delay_time);
+	}
+  //for (int i = 0; i <= 255; i+=10) {
+  //  components.turntable_comms.set_angle(0);
+   // cout << "Value sent to PIC: " << i << endl;
+   // delay(delay_time);
+  //}
 
   cout << "Finished" << endl;
 
